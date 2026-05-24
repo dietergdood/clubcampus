3ec62559f78667dab105640e41cd1345c8848aba
dietergdood <dieter.good@outlook.com>
2026-05-25 01:50:25 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@tabler/icons-webfont/dist/tabler-icons.min.css";
 import App from "./App.jsx";
+
+// Tabler Icons laden - versuche mehrere Pfade
+const loadTablerIcons = () => {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = "https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@2.47.0/tabler-icons.min.css";
+  link.crossOrigin = "anonymous";
+  document.head.appendChild(link);
+};
+loadTablerIcons();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
