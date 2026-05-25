15ad2ffc1e747463da1b5a3a068e8b9a7047b85a
dietergdood <dieter.good@outlook.com>
2026-05-25 02:05:08 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-// Tabler Icons Font direkt einbinden
-// Das Package muss installiert sein: npm install @tabler/icons-webfont
-import "@tabler/icons-webfont/dist/tabler-icons.min.css";
+import "@tabler/icons-webfont/tabler-icons.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
