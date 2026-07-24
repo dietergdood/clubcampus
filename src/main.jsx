335a1d1edbd538414eb83ca172c9efa5dc4d82b6
dietergdood <dieter.good@outlook.com>
2026-07-24 12:44:45 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/cc.css";
+import "./styles/index.css";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
